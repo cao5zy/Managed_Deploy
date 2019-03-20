052d8f6f2261dea62b06be020e042fc61b9685e5
zongying_cao <zongying_cao@163.com>
2019-03-20 23:34:16 +0800

    #7: fix the bug
    Signed-off-by: zongying_cao <zongying_cao@163.com>

--- src/config.py
+++ src/config.py
@@ -1,11 +1,8 @@
-from fn import F
 import os
+from fn import F
 import demjson
-from .assert_util import not_none as assert_not_none
 from slogger import Logger
 from .util import get_project_path, extract_project_path, get_config_path
-from pathlib import Path
-from slogger import Logger
 
 logger = Logger.getLogger(__name__)
 
@@ -26,14 +23,43 @@ config_path: the path of the banyan configuration file
     
     def handle(cfg_json, depended_roles, project_path):
         try:
-            return [y for x in [handle(get_cfg(dep), depended_roles, get_project_path(dep["project_name"])) for dep in cfg_json["dependencies"]] for y in x] + get_roles(cfg_json, logger.title("get_roles_path_recursive").debug(project_path)) \
+            return [y for x in [handle(get_cfg(dep), depended_roles, get_project_path(dep["project_name"])) for dep in cfg_json["dependencies"]] for y in x] \
+                + get_roles(cfg_json, logger.title("get_roles_path_recursive").debug(project_path)) \
             if "dependencies" in cfg_json \
-               else depended_roles + get_roles(logger.title("no dep json").debug(cfg_json), logger.title("no dep path").debug(project_path))
+               else depended_roles + get_roles( \
+                                                logger.title("no dep json").debug(cfg_json) \
+                                                , logger.title("no dep path").debug(project_path) \
+               )
         except Exception:
             logger.error("error happen in handle with project_path:%s\ncfg_json:%s" % (project_path, cfg_json))
             raise
 
-    return handle(demjson.decode_file(config_path), [], extract_project_path(config_path))
+    def put_auth_db_role_first(roles):
+        def change_position(auth_db_roles):
+            assert not auth_db_roles or len(auth_db_roles) == 1, "auth_db is not correctly setup"
+            
+            return auth_db_roles \
+                + roles[0: roles.index(auth_db_roles[0])] \
+                + roles[roles.index(auth_db_roles[0]) + 1:] \
+                if auth_db_roles else roles
+        
+        return change_position( \
+                                list( \
+                                      filter( \
+                                              lambda n: n['role_name'] == 'auth_db' \
+                                              , roles \
+                                      ) \
+                                ) \
+        )
+    
+    return put_auth_db_role_first( \
+                                   handle( \
+                                           demjson.decode_file(config_path) \
+                                           , [] \
+                                           , extract_project_path(config_path) \
+                                   ) \
+                                   )
+                                   
 
 def load_all_dependencies(config_path, handler):
     dependencies = lambda: "dependencies"
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 source .env/bin/activate
 
-.env/bin/pylint src/deploy.py --rcfile pylint.rc
+.env/bin/pylint src/deploy.py \
+		src/config.py --rcfile pylint.rc
 nosetests --nologcapture -s tests -w /root/specialist/Managed_Deploy/src
